765f0c4f9ccc27355e760b558a769ff3d8233c3c
Kyle Goehner <KyleA.Goehner@gmail.com>
2014-11-22 15:48:40 -0600

    Add simple logic for Grid and Cube networks, still need node queuing

--- src/nbody_sim.cpp
+++ src/nbody_sim.cpp
@@ -157,63 +157,162 @@ std::ostream& operator<< ( std::ostream& out, Event& event) {
     return out;
 }
 
-Node* getNextNode(Node *node, Packet *packet, unsigned int topology, size_t num_nodes) {
+Node* getNextNode(Node *node, Packet *packet, unsigned int topology, size_t num_nodes, std::vector<std::vector<std::vector<Node*>*>*> node_mat) {
     Node *next_node = node;
+    VLOG(2) << "NODE: " << *node;
+    VLOG(2) << "PCKT: " << *packet;
     switch (topology) {
         case 0: // RING
             if (packet->x > node->x) {
                 if (packet->x - node->x > num_nodes - packet->x + node->x) {
                     // Get Node with x - 1
+                    if (node->x == 0) {
+                        next_node = node_mat.at(num_nodes-1)->at(node->y)->at(node->z);
+                    } else {
+                        next_node = node_mat.at(node->x-1)->at(node->y)->at(node->z);
+                    }
                 } else {
                     // Get Node with x + 1
+                    if (node->x == num_nodes - 1) {
+                        next_node = node_mat.at(0)->at(node->y)->at(node->z);
+                    } else {
+                        next_node = node_mat.at(node->x+1)->at(node->y)->at(node->z);
+                    }
                 }
             } else {
                 if (node->x - packet->x > num_nodes - node->x + packet->x) {
                     // Get Node with x + 1
+                    if (node->x == num_nodes - 1) {
+                        next_node = node_mat.at(0)->at(node->y)->at(node->z);
+                    } else {
+                        next_node = node_mat.at(node->x+1)->at(node->y)->at(node->z);
+                    }
                 } else {
                     // Get Node with x - 1
+                    if (node->x == 0) {
+                        next_node = node_mat.at(num_nodes-1)->at(node->y)->at(node->z);
+                    } else {
+                        next_node = node_mat.at(node->x-1)->at(node->y)->at(node->z);
+                    }
                 }
             }
             break;
         case 1: // GRID
+            if (node->x != packet->x) {
+                // Move packet in x direction
+                if (packet->x > node->x) {
+                    // Down
+                    next_node = node_mat.at(node->x+1)->at(node->y)->at(node->z);
+                } else {
+                    // Up
+                    next_node = node_mat.at(node->x-1)->at(node->y)->at(node->z);
+                }
+            } else if (node->y != packet->y) {
+                // Move packet in y direction
+                if (packet->y > node->y) {
+                    // Right
+                    next_node = node_mat.at(node->x)->at(node->y+1)->at(node->z);
+                } else {
+                    // Left
+                    next_node = node_mat.at(node->x)->at(node->y-1)->at(node->z);
+                }
+            } else {
+                LOG(FATAL) << "Packet already at destination!";
+            }
             break;
         case 2: // CUBE
+            if (node->x != packet->x) {
+                // Move packet in x direction
+                if (packet->x > node->x) {
+                    // Down
+                    next_node = node_mat.at(node->x+1)->at(node->y)->at(node->z);
+                } else {
+                    // Up
+                    next_node = node_mat.at(node->x-1)->at(node->y)->at(node->z);
+                }
+            } else if (node->y != packet->y) {
+                // Move packet in y direction
+                if (packet->y > node->y) {
+                    // Right
+                    next_node = node_mat.at(node->x)->at(node->y+1)->at(node->z);
+                } else {
+                    // Left
+                    next_node = node_mat.at(node->x)->at(node->y-1)->at(node->z);
+                }
+            } else if (node->z != packet->z) {
+                // Move packet in z direction
+                if (packet->z > node->z) {
+                    // Foreward
+                    next_node = node_mat.at(node->x)->at(node->y)->at(node->z+1);
+                } else {
+                    // Back
+                    next_node = node_mat.at(node->x)->at(node->y)->at(node->z-1);
+                }
+            } else {
+                LOG(FATAL) << "Packet already at destination!";
+            }
             break;
         default:
             LOG(FATAL) << "No topology with id '" << topology  << "' exists.";
     }
+    VLOG(2) << "NEXT: " << *next_node;
     return next_node;
 }
 
-double run_simulation(size_t nodes, size_t data, double compute_time, double latency, unsigned int topology) {
-    size_t nodes_x = nodes;
-    size_t nodes_y = 1;
-    size_t nodes_z = 1;
+double run_simulation(size_t nodes, size_t data, double compute_time, double latency, unsigned int topology, size_t iterations) {
+    size_t nodes_x;
+    size_t nodes_y;
+    size_t nodes_z;
+
+    if (topology == RING) {
+        nodes_x = nodes;
+        nodes_y = 1;
+        nodes_z = 1;
+    } else if (topology == GRID) {
+        nodes_x = nodes;
+        nodes_y = nodes;
+        nodes_z = 1;
+    } else if (topology == CUBE) {
+        nodes_x = nodes;
+        nodes_y = nodes;
+        nodes_z = nodes;
+    }
+
 
     double iteration = 0;
     double simulation_time_s = 0;
     double previous_time_s = 0;
 
+    // Init RNG
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::exponential_distribution<double> compute_distribution(1/compute_time);
+    std::exponential_distribution<double> send_distribution(1/latency);
+
     std::priority_queue<Event*, std::vector<Event*>, CompareEvent> heap;
 
     size_t packets_per_node = ceil(data/nodes);
 
-    std::vector<Node*> node_list;
+    std::vector<std::vector<std::vector<Node*>*>*> node_mat;
 
     // Put initial events in the heap and set worker error delays
     for (size_t x = 0; x < nodes_x; x++) {
+        std::vector<std::vector<Node*>*> *x_vec = new std::vector<std::vector<Node*>*>();
+        node_mat.push_back(x_vec);
         for (size_t y = 0; y < nodes_y; y++) {
+            std::vector<Node*> *y_vec = new std::vector<Node*>();
+            x_vec->push_back(y_vec);
             for (size_t z = 0; z < nodes_z; z++) {
                 Node *node = new Node(x, y, z, 0, packets_per_node);
-                node_list.push_back(node);
-                heap.push(new Event(simulation_time_s + (0.005 * data), COMPUTE, node));
+                y_vec->push_back(node);
+                heap.push(new Event(simulation_time_s + (compute_distribution(generator) * data), COMPUTE, node));
             }
         }
     }
 
     VLOG(2) << "Start Simulation...";
 
-    while (!heap.empty() && iteration < 100) {
+    while (!heap.empty() && iteration < iterations) {
         Event *current_event = heap.top();
         heap.pop();
 
@@ -236,12 +335,18 @@ double run_simulation(size_t nodes, size_t data, double compute_time, double lat
             case 0: // COMPUTE
                 VLOG(2) << "Compute Begin";
                 LOG_IF(ERROR, packet != nullptr) << "Packet was not freed!";
-                for (size_t i = 0; i < node_list.size(); i++) {
-                    if (*node != *(node_list.at(i))) {
-                        Node *dest = node_list[i];
-                        for (size_t i = 0; i < node->total_packets; i++) {
-                            Packet *new_packet = new Packet(dest->x, dest->y, dest->z, node->iteration + 1);
-                            heap.push(new Event(simulation_time_s + 0.00001, PASS_PACKET, node, new_packet));
+                for (size_t x = 0; x < node_mat.size(); x++) {
+                    std::vector<std::vector<Node*>*> *y_vec = node_mat.at(x);
+                    for (size_t y = 0; y < y_vec->size(); y++) {
+                        std::vector<Node*> *z_vec = y_vec->at(y);
+                        for (size_t z = 0; z < z_vec->size(); z++) {
+                            if (*node != *(z_vec->at(z))) {
+                                Node *dest = z_vec->at(z);
+                                for (size_t i = 0; i < node->total_packets; i++) {
+                                    Packet *new_packet = new Packet(dest->x, dest->y, dest->z, node->iteration + 1);
+                                    heap.push(new Event(simulation_time_s + send_distribution(generator), PASS_PACKET, node, new_packet));
+                                }
+                            }
                         }
                     }
                 }
@@ -255,7 +360,7 @@ double run_simulation(size_t nodes, size_t data, double compute_time, double lat
                         node->current_iteration_packets++;
                         if (node->total_packets*(nodes-1) == node->current_iteration_packets - node->total_packets) {
                             VLOG(2) << "Node ready to compute, start compute.";
-                            heap.push(new Event(simulation_time_s + (0.005) * data, COMPUTE));
+                            heap.push(new Event(simulation_time_s + (compute_distribution(generator) * data), COMPUTE));
                         }
                     } else if (packet->iteration == node->iteration + 1) { // Received a packet for the next interation
                         node->next_iteration_packets++;
@@ -265,8 +370,9 @@ double run_simulation(size_t nodes, size_t data, double compute_time, double lat
                     delete packet;
                     packet = nullptr;
                 } else { // Packet needs to be passed on to next node
-                    Node *next_node = getNextNode(node, packet, topology, nodes);
-                    heap.push(new Event(simulation_time_s + 0.00001, PASS_PACKET, next_node, packet));
+                    Node *next_node = getNextNode(node, packet, topology, nodes, node_mat);
+                    VLOG(2) << *next_node;
+                    heap.push(new Event(simulation_time_s + send_distribution(generator), PASS_PACKET, next_node, packet));
                 }
                 VLOG(2) << "Pass Packet End";
                 break;
@@ -279,10 +385,20 @@ double run_simulation(size_t nodes, size_t data, double compute_time, double lat
         delete current_event; // Event's are created with new, so we need to delete them when we're done with them
     }
 
-    //assert(heap.empty());
-    while(!node_list.empty()) {
-        delete node_list.back();
-        node_list.pop_back();
+    LOG_IF(ERROR, !heap.empty());
+    while(!node_mat.empty()) {
+        std::vector<std::vector<Node*>*> *y_vec = node_mat.back();
+        while(!y_vec->empty()) {
+            std::vector<Node*> *z_vec = y_vec->back();
+            while(!z_vec->empty()) {
+                delete z_vec->back();
+                z_vec->pop_back();
+            }
+            delete y_vec->back();
+            y_vec->pop_back();
+        }
+        delete node_mat.back();
+        node_mat.pop_back();
     }
 
     VLOG(1) << "The simulation ended at time: " << simulation_time_s;
@@ -296,12 +412,14 @@ int main(int argc, char **argv) {
     FLAGS_logtostderr = 1;
 
     // Intiate
-    size_t iterations = 10;
-    size_t nodes = 3;
-    size_t data = 15;
-    double compute_time= 2;
-    double latency = 2;
-    unsigned int topology = RING;
+    size_t iterations = 100000;
+    size_t nodes = 5;
+    size_t data = 200;
+    double compute_time= 0.005;
+    double latency = 0.00001;
+    //unsigned int topology = RING;
+    //unsigned int topology = GRID;
+    unsigned int topology = CUBE;
 
     if (argc >= 2) {
         nodes = atoi(argv[1]);
@@ -327,14 +445,13 @@ int main(int argc, char **argv) {
     filename << "nbody_stats" << ".dat";
     outfile.open(filename.str());
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < iterations; j++) {
-            double simulation_time = run_simulation(nodes, data, compute_time, latency, topology);
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 10; j++) {
+            double simulation_time = run_simulation(nodes, data, compute_time, latency, topology, iterations);
             simulation_times.push_back(simulation_time);
         }
 
         // Collect and print statistics.
-
         double sum_time = std::accumulate(simulation_times.begin(), simulation_times.end(), 0.0);
         double min_time = *std::min_element(simulation_times.begin(), simulation_times.end());
         double max_time = *std::max_element(simulation_times.begin(), simulation_times.end());
